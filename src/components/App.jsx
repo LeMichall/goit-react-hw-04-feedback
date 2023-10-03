792ebfb5d9d1a5a85fbff238715541a34d47301e
Michal Leszczyk <Michal Leszczyk>
2023-10-03 18:27:26 +0200

    function name change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { FeedbackOptions } from './Feedback/Feedback';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-export const Apps = () => {
+export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
